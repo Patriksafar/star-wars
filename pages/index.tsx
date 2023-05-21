241012541ad39a437055af8652269d07ebf0b106
Patriksafar <patrik.safar@educanet.cz>
2023-05-21 21:05:05 +0200

    feat: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${monsterrat.className}`}
     >
-      <h1 className="text-4xl font-bold">Star Wars Movies</h1>
+      <h1 className="text-4xl font-bold">Star Wars Movies:</h1>
       <div>
         <div></div>
       </div>
