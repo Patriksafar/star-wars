8c23ffd2a920cf834a6dbc10e4d0358b49efcd3e
Patriksafar <patrik.safar@educanet.cz>
2023-05-21 14:09:47 +0200

    feat: api cleanup

--- pages/api/movies/[id]/index.ts
+++ pages/api/movies/[id]/index.ts
@@ -1,31 +1,5 @@
-// import { movies } from "./data";
-
 import { NextApiRequest, NextApiResponse } from "next";
 import { Movie, movies } from "../../data";
-
-// export async function GET(req: Request, context: any) {
-//   const id = context.params.id;
-
-//   const movie = movies.find((movie) => movie.id === id);
-
-//   return new Response(JSON.stringify(movie));
-// }
-
-export async function POST(req: Request, context: any) {
-  const id = context.params.id;
-  const movie = movies.find((movie) => movie.id === id);
-
-  const body = await req.json();
-  if (movie && body) {
-    const newDescription = body.description;
-    const newTitle = body.title;
-
-    movie.title = newTitle;
-    movie.description = newDescription;
-  }
-
-  return new Response(JSON.stringify(movie));
-}
 
 export default function handler(
   req: NextApiRequest,
